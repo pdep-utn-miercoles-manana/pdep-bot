3c1d9e2488aca99502f1896bd2ec119045ba0fe8
juan mesaglio <juan.mesaglio@gmail.com>
2021-04-05 13:29:54 -0300

    check if exist in db

--- to_database.py
+++ to_database.py
@@ -1,20 +1,25 @@
 import pymongo
 
+"""
+{
+    "lastName": "Mesaglio",
+    "firstName": "Juan",
+    "email": "juan.mesaglio@frba.utn.edu.ar"
+}
+"""
 alumnos = [
-    """
-    {
-        "lastName": "Mesaglio",
-        "firstName": "Juan",
-        "email": "juan.mesaglio@frba.utn.edu.ar"
-    }
-    """
+
 ]
 password = ""
 user = ""
-db=""
-client = pymongo.MongoClient(f"mongodb+srv://{user}:{password}@cluster0.lc5u9.mongodb.net/{db}?retryWrites=true&w=majority")
+db = ""
+client = pymongo.MongoClient(
+    f"mongodb+srv://{user}:{password}@cluster0.lc5u9.mongodb.net/{db}?retryWrites=true&w=majority")
 db = client.pdep.students
 
 for alumno in alumnos:
-    db.insert_one(alumno)
+    if not db.find_one({"email": alumno.get('email')}):
+        db.insert_one(alumno)
+    else:
+        print(f"{alumno.get('email')} ya estaba en la base de datos")
 print("Done!")

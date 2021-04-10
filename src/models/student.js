f300393f2abdd8922c559e49c3b63151284aad78
juan mesaglio <juan.mesaglio@gmail.com>
2021-04-10 12:45:50 -0300

    fix issue

--- src/models/student.js
+++ src/models/student.js
@@ -10,6 +10,7 @@ const Student = Mongoose.Schema({
 });
 
 Student.methods.fullName = function () {
+  this.validateNamesLegnth()
   return _.startCase(_.toLower(`${this.firstName} ${this.lastName}`));
 }
 
@@ -17,6 +18,23 @@ Student.methods.verify = function () {
   this.isVerified = true;
   return this.save();
 };
+
+Student.methods.validateNamesLegnth = function(){
+  var fullName = `${this.firstName} ${this.lastName}`;
+  if (fullName.length >= 32) {
+    let lastNames = this.lastName.split(" ");
+    if (lastNames.length !== 1) {
+      lastNames.pop();
+      this.lastName = lastNames.join(" ").trim();
+      this.validateNamesLegnth();
+    } else {
+      let firstNames = this.firstName.split(" ");
+      firstNames.pop();
+      this.firstName = firstNames.join(" ").trim();
+      this.validateNamesLegnth();
+    }
+  }
+}
 
 Student.statics.MAIL_VERIFIED_ROLE_NAME = config.discord.verifiedMailRoleName;
 
